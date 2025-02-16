afcc855daa3ce38d2ff71e8e0994dd2d90f321f9
dikshitak <dikshitakasodariya26@gmail.com>
2025-02-16 12:11:08 -0500

    login as guest added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
 import Logo from "../ui/Logo";
 import Heading from "../ui/Heading";
+import Button from "../ui/Button";
+import { useLogin } from "../features/authentication/useLogin";
+import Spinner from "../ui/Spinner";
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -14,11 +17,25 @@ const LoginLayout = styled.main`
 `;
 
 function Login() {
+  const { login, isLoading } = useLogin();
+
+  if (isLoading) return <Spinner />;
+
   return (
     <LoginLayout>
       <Logo />
       <Heading as="h4">Log in to your account</Heading>
       <LoginForm />
+      <Button
+        onClick={() =>
+          login({
+            email: import.meta.env.VITE_EMAIL,
+            password: import.meta.env.VITE_PASSWORD,
+          })
+        }
+      >
+        Login as Guest
+      </Button>
     </LoginLayout>
   );
 }
